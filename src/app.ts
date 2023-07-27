60be592b229e0416ab7fc15c062f6a5c1196ba1a
Rodrigo <rodrigo.dsiqueira1@gmail.com>
2023-07-27 15:56:59 -0300

    added basic configuration for ts express API✅🚀

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+
+import { Router, Request, Response } from 'express';
+
+const app = express();
+
+const route = Router()
+
+app.use(express.json())
+
+route.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'hello world with Typescript' })
+})
+
+app.use(route)
+
+app.listen(3333, () => console.log( "\u001b[1;31mServer running on port \u001b[1;32m3333⚡" ))
